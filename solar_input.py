41e5de5c5fde4d3c5cc3c8af369ebfb7d48c2ce1
Khudoteplov <d-n-khudoteplov@yandex.ru>
2020-11-06 11:01:28 +0300

    almost everything fixed

--- solar_input.py
+++ solar_input.py
@@ -19,10 +19,14 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -44,7 +48,15 @@ def parse_star_parameters(line, star):
     **star** — объект звезды.
     """
 
-    pass  # FIXME: not done yet
+    star.type = "star"
+    r, color, m, x, y, vx, vy = line.split()[1:]
+    star.r = r
+    star.color = color
+    star.m = m
+    star.x = x
+    star.y = y
+    star.Vx = vx
+    star.Vy = vy
 
 
 def parse_planet_parameters(line, planet):
@@ -62,7 +74,15 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    planet.type = "star"
+    r, color, m, x, y, vx, vy = line.split()[1:]
+    planet.r = r
+    planet.color = color
+    planet.m = m
+    planet.x = x
+    planet.y = y
+    planet.Vx = vx
+    planet.Vy = vy
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -78,8 +98,8 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME: should store real values
+            print(out_file, obj.type, obj.r, obj.color, obj.m,
+                  obj.x, obj.y, obj.Vx, obj.Vy)
 
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
