9ebbbd1c7987c42ce485e3a775503065428985bc
Khudoteplov <d-n-khudoteplov@yandex.ru>
2020-11-06 11:19:50 +0300

    changed documentation of function

--- solar_input.py
+++ solar_input.py
@@ -6,7 +6,7 @@ from solar_objects import Star, Planet
 
 def read_space_objects_data_from_file(input_filename):
     """Cчитывает данные о космических объектах из файла, создаёт сами объекты
-    и вызывает создание их графических образов
+    и возвращает список объектов
 
     Параметры:
 
